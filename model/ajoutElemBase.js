3d2fc3766533b698c39b89225c69c107462b007a
Altior/ Meikooo <43808598+Altior1@users.noreply.github.com>
2024-12-24 12:07:46 +0100

    implemente les balises nav et footer

---
+++ model/ajoutElemBase.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", afficheElem);
+
+
+function afficheElem () {
+
+
+    afficheNav();
+    afficheFoot();
+}
+async function afficheNav () {
+    const nav = document.getElementById("navbar");
+    let dataNav = await fetch("../vue/navbar.html").then(res => res.text());
+    nav.innerHTML = dataNav;
+}
+
+function afficheFoot () {
+    const foot = document.getElementById("foot");
+    foot.innerHTML = "Copyrigth 2024 Altior1";
+}
