c86c43a236deac7b0ff00ec0c59063a9461368f5
Altior/ Meikooo <43808598+Altior1@users.noreply.github.com>
2025-01-07 15:13:04 +0100

    reprend au 07

--- model/ajoutElemBase.js
+++ model/ajoutElemBase.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", afficheElem);
 function afficheElem () {
     afficheNav();
     afficheFoot();
+    ajoutEffetBtn()
 }
 async function afficheNav () {
     const nav = document.getElementById("navbar");
@@ -14,4 +15,27 @@ async function afficheNav () {
 function afficheFoot () {
     const foot = document.getElementById("foot");
     foot.innerHTML = "Copyrigth 2024 Altior1";
+}
+
+function ajoutEffetBtn () {
+    let btnkisuisje = document.getElementById("btnkisuisje");
+    let btnouvaije = document.getElementById("btnouvaije");
+    btnkisuisje.addEventListener("click", function () {
+        let laDiv = document.getElementById("kisuisje");
+        if (laDiv.style.display === "block") {
+            laDiv.style.display = "none";
+        }
+        else {
+            laDiv.style.display = "block";
+        }
+    });
+    btnouvaije.addEventListener("click", function () {
+        let laDiv = document.getElementById("ouvaije");
+        if (laDiv.style.display === "block") {
+            laDiv.style.display = "none";
+        }
+        else {
+            laDiv.style.display = "block";
+        }
+    });
 }
